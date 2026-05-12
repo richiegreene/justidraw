d75f0728830d241a5f0f657f64b5b7225c916398
richiegreene <richiepgreene@gmail.com>
2026-05-12 00:34:23 -0700

    update help; needs resizing

--- src/help.lua
+++ src/help.lua
@@ -9,21 +9,21 @@ shortcuts
  S: Smooth
  F: Flatten
  H: dodge/burn envelope
+ < and >: selected envelope
+ ?: normalize envelope
  N: envelope tool
  T: transpose/stretch
+ shift+up/down: transpose up/down octave
  U: smudge / vibrato
-
  R: Rectangular selection
  L: Lasso selection
  D: Deselect
  shift+N: toggle selecting notes or vertices
  shift+D: duplicate selection
-
  J: Join notes
  Y: Show harmonic series
  [ and ]: change brush radius
  + and -: change tempo
-
  Space: play/pause
  ctrl+Z: undo
  ctrl+Y / ctrl+shift+Z: redo
@@ -32,5 +32,6 @@ shortcuts
  ctrl+R: render wav
  ctrl+T: cycle between themes
  ctrl+B: cycle between synths
+ shift+R: reverb off/on
  Escape: quit
 ]]
